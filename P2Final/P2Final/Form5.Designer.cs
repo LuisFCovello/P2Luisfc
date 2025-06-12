971199ae0455faa44c9dc86a9f4f4a536a856792
LuisFCovello <luis.covello@sou.unifeb.com.br>
2025-06-12 17:04:11 -0300

    Fiz o forms frmPedidos, aqui vai salvar os pedidos pelo CPF tem 4 txt um para entrar com o usuario outro pra colocar o nome do produto eum pro preço e outro pra quantidade, um botao pra acha o cliente um para adicionar o pedido no dgv um para gravar e outro para excluir

--- P2Final/P2Final/Form5.Designer.cs
+++ P2Final/P2Final/Form5.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace P2Final
 {
-    partial class Form5
+    partial class frmPedidos
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form5";
+            lblNomeCliente = new Label();
+            txtCPFCliente = new TextBox();
+            txtNomeProduto = new TextBox();
+            txtPreco = new TextBox();
+            txtQuantidade = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            lbTotalPedido = new Label();
+            btnBuscarClientes = new Button();
+            btnAdicionar = new Button();
+            btnExcluir = new Button();
+            btnGravar = new Button();
+            dgvItensPedido = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvItensPedido).BeginInit();
+            SuspendLayout();
+            // 
+            // lblNomeCliente
+            // 
+            lblNomeCliente.AutoSize = true;
+            lblNomeCliente.Font = new Font("Segoe UI", 15F);
+            lblNomeCliente.Location = new Point(23, 9);
+            lblNomeCliente.Name = "lblNomeCliente";
+            lblNomeCliente.Size = new Size(158, 28);
+            lblNomeCliente.TabIndex = 0;
+            lblNomeCliente.Text = "Coloque seu CPF";
+            // 
+            // txtCPFCliente
+            // 
+            txtCPFCliente.Location = new Point(23, 40);
+            txtCPFCliente.Name = "txtCPFCliente";
+            txtCPFCliente.Size = new Size(193, 23);
+            txtCPFCliente.TabIndex = 1;
+            // 
+            // txtNomeProduto
+            // 
+            txtNomeProduto.Location = new Point(23, 118);
+            txtNomeProduto.Name = "txtNomeProduto";
+            txtNomeProduto.Size = new Size(193, 23);
+            txtNomeProduto.TabIndex = 2;
+            // 
+            // txtPreco
+            // 
+            txtPreco.Location = new Point(23, 175);
+            txtPreco.Name = "txtPreco";
+            txtPreco.Size = new Size(193, 23);
+            txtPreco.TabIndex = 3;
+            // 
+            // txtQuantidade
+            // 
+            txtQuantidade.Location = new Point(23, 228);
+            txtQuantidade.Name = "txtQuantidade";
+            txtQuantidade.Size = new Size(193, 23);
+            txtQuantidade.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(23, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 21);
+            label2.TabIndex = 5;
+            label2.Text = "Produto";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(23, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(130, 21);
+            label3.TabIndex = 6;
+            label3.Text = "Preço da unidade";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(23, 204);
+            label4.Name = "label4";
+            label4.Size = new Size(91, 21);
+            label4.TabIndex = 7;
+            label4.Text = "Quantidade";
+            // 
+            // lbTotalPedido
+            // 
+            lbTotalPedido.AutoSize = true;
+            lbTotalPedido.Font = new Font("Segoe UI", 12F);
+            lbTotalPedido.Location = new Point(23, 254);
+            lbTotalPedido.Name = "lbTotalPedido";
+            lbTotalPedido.Size = new Size(52, 21);
+            lbTotalPedido.TabIndex = 8;
+            lbTotalPedido.Text = "label5";
+            // 
+            // btnBuscarClientes
+            // 
+            btnBuscarClientes.Location = new Point(23, 314);
+            btnBuscarClientes.Name = "btnBuscarClientes";
+            btnBuscarClientes.Size = new Size(80, 42);
+            btnBuscarClientes.TabIndex = 9;
+            btnBuscarClientes.Text = "Buscar Cliente";
+            btnBuscarClientes.UseVisualStyleBackColor = true;
+            btnBuscarClientes.Click += btnBuscarClientes_Click;
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.Location = new Point(109, 314);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Size = new Size(75, 23);
+            btnAdicionar.TabIndex = 10;
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.UseVisualStyleBackColor = true;
+            btnAdicionar.Click += btnAdicionar_Click;
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.Location = new Point(271, 314);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Size = new Size(75, 23);
+            btnExcluir.TabIndex = 11;
+            btnExcluir.Text = "Excluir";
+            btnExcluir.UseVisualStyleBackColor = true;
+            btnExcluir.Click += btnExcluir_Click;
+            // 
+            // btnGravar
+            // 
+            btnGravar.Location = new Point(190, 314);
+            btnGravar.Name = "btnGravar";
+            btnGravar.Size = new Size(75, 23);
+            btnGravar.TabIndex = 12;
+            btnGravar.Text = "Gravar";
+            btnGravar.UseVisualStyleBackColor = true;
+            btnGravar.Click += btnGravar_Click;
+            // 
+            // dgvItensPedido
+            // 
+            dgvItensPedido.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvItensPedido.Location = new Point(255, 22);
+            dgvItensPedido.Name = "dgvItensPedido";
+            dgvItensPedido.Size = new Size(747, 277);
+            dgvItensPedido.TabIndex = 13;
+            // 
+            // frmPedidos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1035, 429);
+            Controls.Add(dgvItensPedido);
+            Controls.Add(btnGravar);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnAdicionar);
+            Controls.Add(btnBuscarClientes);
+            Controls.Add(lbTotalPedido);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtQuantidade);
+            Controls.Add(txtPreco);
+            Controls.Add(txtNomeProduto);
+            Controls.Add(txtCPFCliente);
+            Controls.Add(lblNomeCliente);
+            Name = "frmPedidos";
+            Text = "Form5";
+            ((System.ComponentModel.ISupportInitialize)dgvItensPedido).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblNomeCliente;
+        private TextBox txtCPFCliente;
+        private TextBox txtNomeProduto;
+        private TextBox txtPreco;
+        private TextBox txtQuantidade;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label lbTotalPedido;
+        private Button btnBuscarClientes;
+        private Button btnAdicionar;
+        private Button btnExcluir;
+        private Button btnGravar;
+        private DataGridView dgvItensPedido;
     }
 }
--- P2Final/P2Final/Form5.cs
+++ P2Final/P2Final/Form5.cs
@@ -7,14 +7,255 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 
 namespace P2Final
 {
-    public partial class Form5 : Form
+    public partial class frmPedidos : Form
     {
-        public Form5()
+        private List<Cliente> listaClientes = new List<Cliente>();
+        private List<Produto> listaProdutos = new List<Produto>();
+        private List<ItemPedido> itensPedido = new List<ItemPedido>();
+        private string caminhoClientes = "clientes.csv";
+        private string caminhoProdutos = "produtos.csv";
+        private string caminhoPedidos = "pedidos.csv";
+        public frmPedidos()
         {
             InitializeComponent();
+            CarregarClientes();
+            CarregarProdutos();
+            AtualizarComboProdutos();
+        }
+        private void CarregarClientes()
+        {
+            if (File.Exists(caminhoClientes))
+            {
+                foreach (var linha in File.ReadAllLines(caminhoClientes))
+                {
+                    var campos = linha.Split(';');
+                    if (campos.Length >= 2)
+                    {
+                        listaClientes.Add(new Cliente { Nome = campos[0], CPF = campos[1] });
+                    }
+                }
+            }
+        }
+        private void CarregarProdutos()
+        {
+            if (File.Exists(caminhoProdutos))
+            {
+                foreach (var linha in File.ReadAllLines(caminhoProdutos))
+                {
+                    var campos = linha.Split(';');
+                    if (campos.Length >= 2)
+                    {
+                        listaProdutos.Add(new Produto
+                        {
+                            Nome = campos[0],
+                            Preco = decimal.Parse(campos[1])
+                        });
+                    }
+                }
+            }
+        }
+
+        private void AtualizarComboProdutos()
+        {
+
+        }
+
+        private void btnBuscarClientes_Click(object sender, EventArgs e)
+        {
+            var cliente = listaClientes.FirstOrDefault(c => c.CPF == txtCPFCliente.Text);
+            if (cliente != null)
+            {
+                lblNomeCliente.Text = cliente.Nome;
+                CarregarPedidosDoCliente(cliente.CPF);
+            }
+            else
+            {
+                MessageBox.Show("Cliente não encontrado!");
+            }
+        }
+             private void CarregarPedidosDoCliente(string cpf)
+        {
+            string pasta = Directory.GetCurrentDirectory();
+            var arquivos = Directory.GetFiles(pasta, $"pedidos_{cpf}_*.csv");
+
+            if (arquivos.Length == 0)
+            {
+                MessageBox.Show("Nenhum pedido encontrado para esse cliente.");
+                return;
+            }
+
+            string caminho = arquivos.OrderByDescending(File.GetLastWriteTime).First();
+
+            itensPedido.Clear();
+
+            foreach (var linha in File.ReadAllLines(caminho))
+            {
+                var campos = linha.Split(';');
+                if (campos.Length >= 4)
+                {
+                    itensPedido.Add(new ItemPedido
+                    {
+                        NomeProduto = campos[0],
+                        Quantidade = int.Parse(campos[1]),
+                        PrecoUnitario = decimal.Parse(campos[2])
+                    });
+                }
+            }
+
+            AtualizarGrid();
+            AtualizarTotal();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            {
+                if (string.IsNullOrWhiteSpace(txtNomeProduto.Text) ||
+                    !int.TryParse(txtQuantidade.Text, out int quantidade) ||
+                    !decimal.TryParse(txtPreco.Text, out decimal preco))
+                {
+                    MessageBox.Show("Preencha todos os campos corretamente!");
+                    return;
+                }
+
+                var item = new ItemPedido
+                {
+                    NomeProduto = txtNomeProduto.Text,
+                    Quantidade = quantidade,
+                    PrecoUnitario = preco
+                };
+
+                itensPedido.Add(item);
+                AtualizarGrid();
+                AtualizarTotal();
+
+                txtNomeProduto.Clear();
+                txtQuantidade.Clear();
+                txtPreco.Clear();
+                txtNomeProduto.Focus();
+            }
+        }
+        private void AtualizarGrid()
+        {
+            dgvItensPedido.DataSource = null;
+            dgvItensPedido.DataSource = itensPedido;
+        }
+
+        private void AtualizarTotal()
+        {
+            decimal total = itensPedido.Sum(item => item.Total);
+            lblTotalPedido.Text = $"Total do Pedido: R$ {total:N2}";
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            {
+                if (string.IsNullOrWhiteSpace(txtCPFCliente.Text))
+                {
+                    MessageBox.Show("Informe o CPF do cliente!");
+                    return;
+                }
+
+                if (itensPedido.Count == 0)
+                {
+                    MessageBox.Show("Nenhum item foi adicionado ao pedido!");
+                    return;
+                }
+
+                try
+                {
+                    string nomeArquivo = $"pedidos_{txtCPFCliente.Text}_{DateTime.Now:yyyyMMddHHmmss}.csv";
+                    MessageBox.Show("Arquivo que será salvo:\n" + nomeArquivo);
+
+                    using (var writer = new StreamWriter(nomeArquivo, false))
+                    {
+                        foreach (var item in itensPedido)
+                        {
+                            writer.WriteLine($"{item.NomeProduto};{item.Quantidade};{item.PrecoUnitario};{item.Total}");
+                        }
+                    }
+
+                    MessageBox.Show("Pedido salvo com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao salvar pedido: " + ex.Message);
+                }
+            }
+        }
+        public class ItemPedido
+        {
+            public string NomeProduto { get; set; }
+            public decimal PrecoUnitario { get; set; }
+            public int Quantidade { get; set; }
+            public decimal Total => PrecoUnitario * Quantidade;
+        }
+        public class Produto
+        {
+            public string Nome { get; set; }
+            public decimal Preco { get; set; }
+        }
+        private void txtPreco_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvItensPedido_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtQuantidade_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (dgvItensPedido.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecione um item para excluir.");
+                return;
+            }
+
+            var itemSelecionado = (ItemPedido)dgvItensPedido.SelectedRows[0].DataBoundItem;
+
+            var confirm = MessageBox.Show(
+                $"Deseja excluir o item '{itemSelecionado.NomeProduto}'?",
+                "Confirmação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (confirm == DialogResult.Yes)
+            {
+                itensPedido.Remove(itemSelecionado);
+
+                AtualizarGrid();
+                AtualizarTotal();
+
+                if (!string.IsNullOrWhiteSpace(txtCPFCliente.Text))
+                {
+                    var cpf = txtCPFCliente.Text.Trim();
+                    var arquivos = Directory.GetFiles(Directory.GetCurrentDirectory(), $"pedidos_{cpf}_*.csv");
+
+                    if (arquivos.Length > 0)
+                    {
+                        string ultimoArquivo = arquivos.OrderByDescending(File.GetLastWriteTime).First();
+
+                        using (var writer = new StreamWriter(ultimoArquivo, false))
+                        {
+                            foreach (var item in itensPedido)
+                            {
+                                writer.WriteLine($"{item.NomeProduto};{item.Quantidade};{item.PrecoUnitario};{item.Total}");
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
